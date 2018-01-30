b35a771b1cd0d72c076e6f0effb725bbc7588d00
Danil Guskov <d.guskov@corp.mail.ru>
2018-01-30 21:30:50 +0300

    more plans

---
+++ multi-package-sample/plan.ps1
@@ -0,0 +1,20 @@
+$pkg_name="multi-package-sample"
+$pkg_origin="qago"
+$pkg_license=@("LGPL")
+$pkg_description="mult package sample"
+$pkg_maintainer="QAGO Maintainers"
+$pkg_source="_no_pkg_source_"
+$pkg_version="1.0"
+$pkg_deps=@("qago/python/3.6.4/20180130140127","qago/ffmpeg/20170411-f1d80bc/20180130172218","core/powershell")
+
+function Invoke-Download { }
+
+function Invoke-Unpack {
+}
+
+function Set-PackageVersion {
+}
+
+function Invoke-Install { }
+
+function Invoke-Verify { }
