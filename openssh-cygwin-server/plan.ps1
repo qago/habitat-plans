e9a32425d650266d158a169131f27f0f440faeaa
Danil Guskov <d.guskov@corp.mail.ru>
2018-02-02 20:39:19 +0300

    cygwin openssh server

---
+++ openssh-cygwin-server/plan.ps1
@@ -0,0 +1,11 @@
+$pkg_name="openssh-cygwin-server"
+$pkg_origin="qago"
+$pkg_license=@("LGPL")
+$pkg_description="openssh-cygwin-server"
+$pkg_maintainer="QAGO Maintainers"
+$pkg_source="_no_pkg_source_"
+$pkg_version="1.0"
+$pkg_deps=@("qago/openssh-cygwin")
+
+function Invoke-Download { }
+function Invoke-Verify { }
