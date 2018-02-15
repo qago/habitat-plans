6681bba74462f857f3f6285ccd045ce3b569db05
Danil Guskov <d.guskov@corp.mail.ru>
2018-02-15 21:35:11 +0300

    more plans

--- openssh-cygwin-server/plan.ps1
+++ openssh-cygwin-server/plan.ps1
@@ -5,7 +5,8 @@ $pkg_description="openssh-cygwin-server"
 $pkg_maintainer="QAGO Maintainers"
 $pkg_source="_no_pkg_source_"
 $pkg_version="1.0"
-$pkg_deps=@("qago/openssh-cygwin")
+$pkg_deps=@("qago/openssh-cygwin", "core/powershell")
+$pkg_svc_user="Administrator"
 
 function Invoke-Download { }
 function Invoke-Verify { }
