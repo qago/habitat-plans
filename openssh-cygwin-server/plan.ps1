73078a350f6592d08b3f8ea1692032f98168bfbf
Danil Guskov <d.guskov@corp.mail.ru>
2018-06-05 23:36:33 +0300

    fix

--- openssh-cygwin-server/plan.ps1
+++ openssh-cygwin-server/plan.ps1
@@ -3,7 +3,6 @@ $pkg_origin="qago"
 $pkg_license=@("LGPL")
 $pkg_description="openssh-cygwin-server"
 $pkg_maintainer="QAGO Maintainers"
-$pkg_source="_no_pkg_source_"
 $pkg_version="1.0"
 $pkg_deps=@("qago/openssh-cygwin", "core/powershell")
 $pkg_svc_user="Administrator"
