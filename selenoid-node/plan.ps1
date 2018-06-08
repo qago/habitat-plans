b022b03231f3e7b2eaca3e83504e89d171c2205d
Your Name <you@example.com>
2018-06-08 21:08:50 +0300

    selenoid node rewrite

--- selenoid-node/plan.ps1
+++ selenoid-node/plan.ps1
@@ -5,6 +5,7 @@ $pkg_description="selenoid windows node"
 $pkg_maintainer="QAGO Maintainers"
 $pkg_version="1.0.1"
 $pkg_deps=@("qago/selenoid/1.6.0")
+$pkg_svc_user="Administrator"
 
 $pkg_exports=@{
   settings="settings"
