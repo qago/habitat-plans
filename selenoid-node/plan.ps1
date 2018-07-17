c6502ec67bc06de91c848b8869998e6dd01dc2af
Your Name <you@example.com>
2018-07-17 17:17:39 +0300

    selenoid-node windows fixes

--- selenoid-node/plan.ps1
+++ selenoid-node/plan.ps1
@@ -3,7 +3,7 @@ $pkg_origin="qago"
 $pkg_license=@("LGPL")
 $pkg_description="selenoid windows node"
 $pkg_maintainer="QAGO Maintainers"
-$pkg_version="1.0.5"
+$pkg_version="1.0.6"
 $pkg_deps=@("qago/selenoid/1.6.0")
 $pkg_svc_user="Administrator"
 
