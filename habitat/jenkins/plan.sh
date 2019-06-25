b85c77a01a6d0ddf3c4355efd98a3bdb7ef48354
Danil Guskov <d.guskov@corp.mail.ru>
2019-06-25 16:40:51 +0300

    jenkins pkg

---
+++ habitat/jenkins/plan.sh
@@ -0,0 +1,27 @@
+pkg_name=jenkins
+pkg_origin=qago
+pkg_version=2.150.1
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
+pkg_license=('MIT')
+pkg_upstream_url="https://jenkins.io/"
+pkg_source="http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war"
+pkg_shasum="7a38586d5a3a1a83498809a83715728bb2f01b58a7dd3a88366f076efdaf6669"
+pkg_deps=(core/jre8 core/curl)
+pkg_exports=(
+  [port]=jenkins.http.port
+)
+pkg_exposes=(port)
+pkg_svc_user="root"
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  cp "${HAB_CACHE_SRC_PATH}"/"${pkg_filename}" "${pkg_prefix}"/jenkins.war
+}
