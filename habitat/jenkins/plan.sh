8103807c07c315bb3ab074c8fffae0604185403f
Семенов Евгений <e.semenov@corp.mail.ru>
2019-09-04 13:24:57 +0300

    Update jenkins plan to -> 2.176.3

--- habitat/jenkins/plan.sh
+++ habitat/jenkins/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=jenkins
 pkg_origin=qago
-pkg_version=2.150.1
-pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_version=2.176.3
+pkg_maintainer="QAGO members"
 pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
 pkg_license=('MIT')
 pkg_upstream_url="https://jenkins.io/"
 pkg_source="http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war"
-pkg_shasum="7a38586d5a3a1a83498809a83715728bb2f01b58a7dd3a88366f076efdaf6669"
+pkg_shasum="9406c7bee2bc473f77191ace951993f89922f927a0cd7efb658a4247d67b9aa3"
 pkg_deps=(core/jre8 core/curl)
 pkg_exports=(
   [port]=jenkins.http.port
